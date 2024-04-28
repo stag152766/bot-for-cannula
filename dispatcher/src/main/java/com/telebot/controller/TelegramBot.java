b039ee0eb84c83522309991401ef086c7ba1ad4a
stag <stag152766@gmail.com>
2024-04-28 23:50:05 +0700

    Create scheduler

--- dispatcher/src/main/java/com/telebot/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/telebot/controller/TelegramBot.java
@@ -1,7 +1,6 @@
 package com.telebot.controller;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,14 +8,22 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Log4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private long userId;
 
     @Value("${bot.name}")
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+    @Value("${cannula.delay}")
+    private int DELAY;
 
     @Override
     public String getBotUsername() {
@@ -30,22 +37,34 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var originalMessage = update.getMessage();
-        log.debug(originalMessage.getText());
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
-        var response = new SendMessage();
-        response.setChatId(originalMessage.getChatId().toString());
-        response.setText("Hello from bot!");
-        sendAnswerMessage(response);
+            if (messageText.equals("/set_cannula")) {
+                userId = update.getMessage().getFrom().getId();
+                setReminder();
+                sendTextMessage(chatId, "Таймер запущен. Я напомню вам о необходимости поменять канюлю через 3 дня.");
+            }
+        }
     }
 
-    public void sendAnswerMessage(SendMessage message) {
-        if (message != null) {
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                log.error(e);
-            }
+    private void setReminder() {
+        scheduler.schedule(() -> sendReminder(userId), DELAY, TimeUnit.SECONDS);
+    }
+
+    private void sendReminder(long userId) {
+        sendTextMessage(userId, "Настало время менять канюлю!");
+    }
+
+    private void sendTextMessage(long userId, String message) {
+        var response = new SendMessage();
+        response.setChatId(userId);
+        response.setText(message);
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            log.error(e);
         }
     }
 }
